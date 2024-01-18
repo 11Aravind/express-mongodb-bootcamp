3b27e0d04f6a62525b68badc12b48ef8aab0a08b
11Aravind <aravindsreenandanam11@gmail.com>
2024-01-18 11:19:50 +0530

    bcryptjs download and post request was handled

--- Backend/app.js
+++ Backend/app.js
@@ -5,6 +5,7 @@ import router from "./routes/user_routes.js";
 
 
 const app=express();
+app.use(express.json())
 app.use(router); 
 //app.use("/api/user",router)
 
--- Backend/controllers/User-middleware.js
+++ Backend/controllers/User-middleware.js
@@ -1,16 +1,18 @@
 import User from "../models/User.js";
+import bcrypt from 'bcryptjs';
 export const signup = async (req, res, next) => {
     const { name, email, password } = req.body;
+    const hashedPassword=bcrypt.hashSync(password)
     const user = new User({
         name,
         email,
-        password
+        password:hashedPassword
     });
     try {
 
         await user.save();
     } catch (err) {
-        console.log(err);
+        return console.log(err);
     }
     return res.status(201).json({user})
 }
