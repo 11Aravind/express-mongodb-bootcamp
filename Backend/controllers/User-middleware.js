4e6abec3566f261e1377b8bf6458058fe46ecb61
11Aravind <aravindsreenandanam11@gmail.com>
2024-01-20 09:35:59 +0530

    login middleware was created

--- Backend/controllers/User-middleware.js
+++ Backend/controllers/User-middleware.js
@@ -41,7 +41,20 @@ export const signupMiddleware = async (req, res, next) => {
     }
     return res.status(201).json({ user })
 }
+
+
 export const loginMiddleware = async (req, res, next) => {
 
-
+    const { email, password } = req.body;
+    let existingUser;
+    try {
+        existingUser = await User.findOne({ email });
+    }
+    catch (error) { return console.log(error); }
+    if (!existingUser)
+        return res.status(404).json({ message: "coudnt find user e-mail" })
+    const isPasswordCorrect=bcrypt.compareSync(password,existingUser.password)
+    if(!isPasswordCorrect)
+        return res.status(404).json({message:"password was wrong "})
+    return res.status(200).json({message:"Login was successful"})
 }
