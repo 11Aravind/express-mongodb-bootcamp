c9b6db3035ea711c714d3847665be522ac485dc4
11Aravind <aravindsreenandanam11@gmail.com>
2024-01-23 17:49:27 +0530

    blog was edited

--- Backend/controllers/Blog-Middleware.js
+++ Backend/controllers/Blog-Middleware.js
@@ -1,32 +1,45 @@
 import Blog from "../models/Blog.js"
-export const getAllBlogs=async(req,res,next)=>{
-let blogDetails;
-try{
-    blogDetails=await Blog.find();
-}
-catch(err){
-    return console.log(err);
-}
-if(blogDetails.length===0)
-    return res.status(404).json({message:"Blog was empty"})
-return res.status(200).json({blogDetails});
+export const getAllBlogs = async (req, res, next) => {
+    let blogDetails;
+    try {
+        blogDetails = await Blog.find();
+    }
+    catch (err) {
+        return console.log(err);
+    }
+    if (blogDetails.length === 0)
+        return res.status(404).json({ message: "Blog was empty" })
+    return res.status(200).json({ blogDetails });
 }
 
-export const addBlog=async(req,res,next)=>{
-const {title,description,image,user_id}=req.body;
-const newBlog=new Blog({
-    title,
-    description,
-    image,
-    user_id
-});
-try{
-   await newBlog.save();
-}catch(error){
-    return console.log(error);
+export const addBlog = async (req, res, next) => {
+    const { title, description, image, user_id } = req.body;
+    const newBlog = new Blog({
+        title,
+        description,
+        image,
+        user_id
+    });
+    try {
+        await newBlog.save();
+    } catch (error) {
+        return console.log(error);
+    }
+    return res.status(200).json({ message: "New blog was added" })
 }
-return res.status(200).json({message:"New blog was added"})
-}
-const update=async(req,res,next)=>{
-const blogId=req.params.id;
+export const updateBlog = async (req, res, next) => {
+    const { title, description } = req.body;
+    const blogId = req.params.id;
+    let blog;
+    try {
+        //findByIdAndUpdate(id,update cheyyenda value) ite id find cheythe value update akkikollum
+        blog = await Blog.findByIdAndUpdate(blogId,
+            { title, description }
+        )
+    } catch (error) {
+       return  console.log(error);
+    }
+    if (!blog)
+        return res.status(500).json({ message: "Unable to update" })
+    return res.status(200).json({ blog })
 }
--- Backend/routes/blog-routes.js
+++ Backend/routes/blog-routes.js
@@ -1,7 +1,7 @@
 import express from "express";
-import {getAllBlogs,addBlog} from "../controllers/Blog-Middleware.js"
+import {getAllBlogs,addBlog,updateBlog} from "../controllers/Blog-Middleware.js"
 const blogRouter=express.Router()
 blogRouter.get("/",getAllBlogs)
 blogRouter.post("/add",addBlog)
-blogRouter.put("/update/:id",addBlog)
+blogRouter.put("/update/:id",updateBlog)
 export default blogRouter;
