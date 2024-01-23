24edf1cdc9c67507c96fdc30e4002f09a79eb636
Aravind A.S <98075912+11Aravind@users.noreply.github.com>
2024-01-22 20:53:51 -0800

    Update Product-middleware.js

--- Backend/controllers/Product-middleware.js
+++ Backend/controllers/Product-middleware.js
@@ -1,1 +1,17 @@
+import mongoose from "mongoose";
+import Product from "./Models/Product";
+export const getProductDetails=async(req,res,next)=>{
+  return(
+    try{
+    const productDetails=Product.find();
+    }
+    catch(error){
+    return console.log(error);
+    }
+  );
+}
+export const saveProduct=async(req,res,next)=>{
+const {name,description,category,subCategory,image,oldPrice,newPrice,status}=req.body;
+}
 
+})
