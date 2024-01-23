e072861773635d2a4dcd9d10b2dc04bf9231ab9c
Aravind A.S <98075912+11Aravind@users.noreply.github.com>
2024-01-22 20:38:06 -0800

    Update Product.js

--- Backend/models/Product.js
+++ Backend/models/Product.js
@@ -1,1 +1,38 @@
-
+import mongoose from "mongoose";
+const Schema=mongoose.Schema;
+const productSchema=new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+  
+    category:{
+        type:String,
+        required:true
+    },
+  subCategory:{
+  type:String,
+  required:true,
+  },
+    image:{
+        type:String,
+        required:true
+    },
+oldPrice:{
+  type:Double,
+  required:true
+},
+  newPrice:{
+  type:Double,
+  required:true
+},
+  status:{
+  type:Boolean,
+  }
+  
+})
+export default  mongoose.model("Blog",blogSchema); //blog collection schema create akkunnu
