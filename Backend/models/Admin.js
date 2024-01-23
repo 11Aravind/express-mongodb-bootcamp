038d862210bdbbfa7c597a8626206c2969115623
Aravind A.S <98075912+11Aravind@users.noreply.github.com>
2024-01-22 20:41:24 -0800

    Update Admin.js

--- Backend/models/Admin.js
+++ Backend/models/Admin.js
@@ -1,1 +1,17 @@
+//define a collection or table
+import mongoose from "mongoose";
 
+const Schema = mongoose.Schema;
+const adminSchema=new Schema({
+    email:{
+        type:String,
+        require:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        minlength:6,
+        required:true
+    }
+});
+export default mongoose.model("Admin",adminSchema);
