be1fbf332a51d06f59c229537953ec40a9195628
11Aravind <aravindsreenandanam11@gmail.com>
2024-02-11 18:38:04 +0530

    some main file changed

--- Backend/routes/blog-routes.js
+++ Backend/routes/blog-routes.js
@@ -1,11 +1,9 @@
 import express from "express";
-import upload from "../controllers/multer-middleware";
+import multer from "multer";
+import upload from "../controllers/multer-middleware.js";
 import { getAllBlogs, addBlog, updateBlog } from "../controllers/Blog-Middleware.js"
 const blogRouter = express.Router()
 blogRouter.get("/", getAllBlogs)
-blogRouter.post("/add", upload.single('file'), (req, res, next) => {
-    // res.json({ message: 'File uploaded successfully!' });
-    res.json({message:req.body})
-})//addBlog
+blogRouter.post("/add",upload.single('image'),addBlog)//addBlog
 blogRouter.put("/update/:id", updateBlog)
 export default blogRouter;
