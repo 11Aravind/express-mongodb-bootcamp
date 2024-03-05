a95de465ff6b980f192bdcdfd684e8c9856a821e
11Aravind <aravindsreenandanam11@gmail.com>
2024-03-05 11:36:23 +0530

    some changes was added

--- Backend/models/Product.js
+++ Backend/models/Product.js
@@ -1,38 +1,34 @@
 import mongoose from "mongoose";
-const Schema=mongoose.Schema;
-const productSchema=new Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    description:{
-        type:String,
-        required:true
-    },
-  
-    category:{
-        type:String,
-        required:true
-    },
-  subCategory:{
-  type:String,
-  required:true,
+const Schema = mongoose.Schema;
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: true
   },
-    image:{
-        type:String,
-        required:true
-    },
-oldPrice:{
-  type:Double,
-  required:true
-},
-  newPrice:{
-  type:Double,
-  required:true
-},
-  status:{
-  type:Boolean,
+  image: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+
+  oldPrice: {
+    type: Number,
+    required: true
+  },
+  newPrice: {
+    type: Number,
+    required: true
+  },
+  status: {
+    type: Boolean,
+  },
+  category_id:{
+    type:Schema.Types.ObjectId,
+    ref:"Category"
   }
-  
+
 })
-export default  mongoose.model("Product", productSchema); 
+export default mongoose.model("Product", productSchema); 
